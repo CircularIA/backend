fba396aa34b590c04ebb0c7ae732be9954148433
javier-tralma <javier.tralma@sansano.usm.cl>
2023-08-03 17:43:24 -0400

    feat: main backend, using routes and cors

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connection = require('./db');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+
+// Connect to database
+connection();
+
+const corsOptions = {
+    credentials: true,
+    origin: 'http://localhost:5173', // Cambia esto con la URL de tu cliente de React
+};
+
+
+// Middleware
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
