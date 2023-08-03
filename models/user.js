8481af697eb70dcf4cacd16a609fc68b640fe07b
javier-tralma <javier.tralma@sansano.usm.cl>
2023-08-03 17:41:24 -0400

    feat: user model from mongo

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const moongose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
+
+const userSchema = new moongose.Schema({
+    fullName:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    companyName:{
+        type: String,
+        required: true,
+    },
+});
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_KEY, {expiresIn: '1m'});
+    return token;
+}
+
+const User = moongose.model('User', userSchema);
+
+const validate = (data) => {
+    const Schema = Joi.object({
+        fullName: Joi.string().required().label('Full Name'),
+        email: Joi.string().required().email().label('Email'),
+        password: passwordComplexity().required().label('Password'),
+        companyName: Joi.string().required().label('Company Name'),
+    });
+    return Schema.validate(data);
+}
+
+module.exports = { User, validate };
