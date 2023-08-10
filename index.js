1ef271791a895519db3f979fce061f78be93ef93
javier-tralma <javier.tralma@sansano.usm.cl>
2023-08-10 14:22:13 -0400

    feat: verify route for validate login

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const connection = require('./db');
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
+const verifyRoutes = require('./routes/verify');
 
 // Connect to database
 connection();
@@ -24,6 +25,7 @@ app.use(cookieParser());
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/verify', verifyRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
