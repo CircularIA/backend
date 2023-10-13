f375112f9b87d63fa55e255abc8ec10a26dba83b
Nightmare8 <juan.caniguante@sansano.usm.cl>
2023-10-13 18:19:53 -0300

    add companie model and branch

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ const userSchema = new moongose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id: this._id}, process.env.JWT_KEY, {expiresIn: '1m'});
+    const token = jwt.sign({_id: this._id}, process.env.JWT_KEY, {expiresIn: '3h'});
     return token;
 }
 
