636b02a5afe1b5f856526e7f991ba50a618b5cda
Javier Tralma <javier.tralma@sansano.usm.cl>
2024-04-07 23:18:31 -0400

    refactor: gramatical correction "category" and subcategory implemented

--- models/ListInputDat.js
+++ models/ListInputDat.js
@@ -7,10 +7,29 @@ const ListInputDatSchema = new Schema({
 	description: { type: String },
 	measurement: { type: String },
 	//Norm is determined by indicator
-	categorie: {
+	category: {
 		type: String,
 		required: true,
 		enum: ["Ambiental", "Social", "Economica"],
+	},
+	subcategory: {
+		type: String,
+		required: true,
+		enum: [
+			"Salida y valorización de Residuos, Productos y subproductos",
+			"Agua",
+			"Energía",
+			"Huella de carbono de salida",
+			"Entrada de suministros",
+			"Productividad circular de material",
+			"Porcentaje de ingreso por acciones circulares",
+			"Porcentaje inversión en circularidad",
+			"Empleo verde",
+			"Porcentaje de empleos circulares",
+			"Educación ambiental interna",
+			"Porcentaje de participación femenina",
+			"Social explícito",
+		],
 	},
 });
 
@@ -19,9 +38,26 @@ ListInputDatSchema.statics.validateNewInputDat = async (inputDat) => {
 		name: Joi.string().required(),
 		description: Joi.string(),
 		measurement: Joi.string(),
-		categorie: Joi.string()
+		category: Joi.string()
 			.required()
 			.valid("Ambiental", "Social", "Economica"),
+		subcategory: Joi.string()
+			.required()
+			.valid(
+				"Salida y valorización de Residuos, Productos y subproductos",
+				"Agua",
+				"Energía",
+				"Huella de carbono de salida",
+				"Entrada de suministros",
+				"Productividad circular de material",
+				"Porcentaje de ingreso por acciones circulares",
+				"Porcentaje inversión en circularidad",
+				"Empleo verde",
+				"Porcentaje de empleos circulares",
+				"Educación ambiental interna",
+				"Porcentaje de participación femenina",
+				"Social explícito"
+			),
 	});
 };
 
